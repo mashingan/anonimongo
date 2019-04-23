a0e3ef41d56f76b67526fa377bab18dea60adcf3
mashingan <rahmat.d.ruffy@gmail.com>
2019-04-23 17:56:51 +0700

    remove threadpool, add lock, exports field and procs

--- pool.nim
+++ pool.nim
@@ -1,105 +1,146 @@
-import deques, math, tables, threadpool
+import deques, math, tables, selectors
+import sugar, options, locks, times
 import bson, wire
 
 type
-  Connection = object
-    socket: AsyncSocket
-    id: uint
-  Pool = ref object
-    connections: TableRef[int, Connection]
-    available: Deque[int]
-    connCount: int
-    #event: SelectEvent
-    #selector: Selector
+  Connection* = object
+    socket* {.guard: lock.}: AsyncSocket
+    id*: uint
+    lock*: Lock
 
-proc initConnection(id = 0): Connection =
-  result.socket = newAsyncSocket()
+  Pool* = ref object
+    connections: TableRef[int, Connection]
+    available*: Deque[int]
+    event: SelectEvent
+    selector: Selector[bool]
+
+template withLock*(l, ops: untyped): untyped =
+  {.locks: [l.lock].}:
+    ops
+
+proc initConnection*(id = 0): Connection =
+  initLock result.lock
+  withLock result:
+    result.socket = newAsyncSocket()
   result.id = id.uint
 
 proc contains*(p: Pool, i: int): bool =
   i in p.connections
 
-proc `[]`(p: Pool, i: int): Connection =
+proc `[]`*(p: Pool, i: int): Connection =
   p.connections[i]
 
-proc `[]=`(p: Pool, i: int, c: Connection) =
+proc `[]=`*(p: Pool, i: int, c: Connection) =
   p.connections[i] = c
 
-proc initPool(size = 16): Pool =
+proc initPool*(size = 16): Pool =
   new result
   result.connections = newTable[int, Connection](size)
-  for i in 0 ..< 16:
-    result[i] = i.initConnection
-
   result.available = initDeque[int](size.nextPowerOfTwo)
-  #[
+  for i in 0 ..< size:
+    result[i] = i.initConnection
+    result.available.addFirst i
+
   result.event = newSelectEvent()
   result.selector = newSelector[bool]()
-
   result.selector.registerEvent(result.event, true)
-  ]#
 
-proc getConn(p: Pool): Future[Connection] =
-  result = newFuture[Connection]("pool.getConn")
-  proc retrieveConn(thepool: Pool): Future[Connection] {.async.} =
-    while thepool.available.len == 0:
-      discard sleepAsync 10
 
-    return thepool[thepool.available.popFirst]
-
+proc getConn*(p: Pool): Future[Option[Connection]] =
+  result = newFuture[Option[Connection]]("pool.getConn")
   try:
-    let conn = p.retrieveConn
-    p.connCount = (p.connCount + 1) mod p.connections.len
-    result = p.retrieveConn
+    dump p.available
+    if p.available.len == 0:
+      result.complete none(Connection)
+    else:
+      result.complete p[p.available.popFirst].some
+  except IndexError as ie:
+    result.fail newException(IndexError, "Error getConn: " & ie.msg)
   except Exception as exc:
     result.fail exc
 
-proc connect(p: Pool, address: string, port: int) {.async.} =
-  #var conn = newseq[Future[void]](p.connections.len)
+proc connect*(p: Pool, address: string, port: int) {.async.} =
   for i, c in p.connections:
-    await c.socket.connect(address, Port port)
+    withLock c:
+      await c.socket.connect(address, Port port)
     echo "connection: ", i, " is connected"
 
-proc close(p: Pool) =
+proc close*(p: Pool) =
   for _, c in p.connections:
-    if not c.socket.isClosed:
-      close c.socket
-      echo "connection: ", c.id, " is closed"
+    withLock c:
+      if not c.socket.isClosed:
+        close c.socket
+        echo "connection: ", c.id, " is closed"
 
-proc endConn(p: Pool, i: Positive) =
+proc endConn*(p: Pool, i: Positive) =
   p.available.addLast i.int
 
-proc handshake(s: AsyncSocket, db: string) {.async.} =
+proc handshake(s: AsyncSocket, db: string, id: int32) {.async.} =
   let q = bson({
-    application: { name: "test connection pool" },
-    driver: {
-      name: "anonimongo - nim mongo driver",
-      version: "0.0.4",
-    },
-    os: {
-      "type": "Windows",
-      architecture: "amd64"
+    isMaster: 1,
+    client: {
+      application: { name: "test connection pool: " & $id },
+      driver: {
+        name: "anonimongo - nim mongo driver",
+        version: "0.0.4",
+      },
+      os: {
+        "type": "Windows",
+        architecture: "amd64"
+      }
     }
   })
+  echo "Handshake id: ", id
+  dump q
   var stream = newStringStream()
-  discard stream.prepareQuery(0, 0, opQuery.int32, 0, db,
+  discard stream.prepareQuery(id, 0, opQuery.int32, 0, db,
     0, 1, q)
   await s.send stream.readAll
   look(await s.getReply)
 
+proc query1(s: AsyncSocket, db: string, id: int32) {.async.} =
+  look(await queryAck(s, id, db, "reporter", limit = 1))
+
 when isMainModule:
   let poolSize = 16
+  let loopsize = poolsize * 2
   var pool = initPool(poolSize)
   waitFor pool.connect("localhost", 27017)
 
-  proc toHandshake() {.gcsafe, async.} =
-      var conn = await pool.getConn
-      await conn.socket.handshake("reporting.$cmd")
-      echo "end conn: ", conn.id
-      pool.endConn conn.id
+  proc toHandshake(i: int): Future[bool] {.async.} =
+      echo "spawning: ", i
+      let conn = await pool.getConn
+      if conn.isNone:
+        result = false
+        return
+      var actconn = conn.get
+      dump actconn.id
+      #await actconn.socket.handshake("reporting.$cmd", actconn.id.int32)
+      withLock actconn:
+        await actconn.socket.query1("reporting", actconn.id.int32)
+      echo "end conn: ", actconn.id
+      pool.endConn actconn.id
+      #dump pool.available
+      result = true
 
-  for _ in 1 .. poolSize*2:
-    discard(spawn toHandShake())
+  #[
+  var futhandshake = newseq[Future[bool]](poolSize*2)
+  for i in 0 ..< poolSize*2:
+    futhandshake[i] = toHandShake(i)
+  try:
+    discard(waitFor all(futhandshake))
+  except IndexError as ie:
+    echo ie.msg
+    ]#
 
-  sync()
+  let starttime = cpuTime()
+  var futhandshake = newseq[bool](loopSize)
+  var count = 0
+  while count < loopSize:
+    let hshake = waitFor toHandShake(count)
+    if hshake == false: continue
+    futhandshake[count] = hshake
+    inc count
+  echo "ended loop at: ", cpuTime() - starttime
+
   close pool
