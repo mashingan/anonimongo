01bf95029f53b2f23e5680da13c219d8694a207a
mashingan <rahmat.d.ruffy@gmail.com>
2020-02-08 12:49:44 +0700

    add dropAll..., grantPriv... and grantRolesToRole

--- rolemgmt.nim
+++ rolemgmt.nim
@@ -12,10 +12,34 @@ proc createRole*(db: Database, name: string, privileges, roles: seq[BsonDocument
   if authRestrict.len >= 0:
     q["authenticationRestriction"] = authRestrict.map toBson
   q.addWriteConcern(db, wt)
+  result = await db.proceed(q, "admin")
+
+proc dropRole*(db: Database, role: string, wt = bsonNull()):
+  Future[(bool, string)]{.async.} =
+  var q = bson({ dropRole: role })
+  q.addWriteConcern(db, wt)
   result = await db.proceed(q)
 
-proc dropRole*(db: Database, name: string, wt = bsonNull()):
-  Future[(bool, string)]{.async.} =
-  var q = bson({ dropRole: name })
+proc dropAllRolesFromDatabase*(db: Database, wt = bsonNull()):
+    Future[(bool, string)] {.async.} =
+  var q = bson({ dropAllRolesFromDatabase: 1 })
+  q.addWriteConcern(db, wt)
+  result = await db.proceed(q)
+
+proc grantPrivilegesToRole*(db: Database, role: string, privileges: seq[BsonDocument],
+  wt = bsonNull()): Future[(bool, string)] {.async.} =
+  var q = bson({
+    grantPrivilegesToRole: role,
+    privileges: privileges.map toBson,
+  })
+  q.addWriteConcern(db, wt)
+  result = await db.proceed(q)
+
+proc grantRolesToRole*(db: Database, role: string, roles: seq[BsonDocument],
+  wt = bsonNull()): Future[(bool, string)] {.async.} =
+  var q = bson({
+    grantRolesToRole: role,
+    roles: roles.map toBson
+  })
   q.addWriteConcern(db, wt)
   result = await db.proceed(q)
