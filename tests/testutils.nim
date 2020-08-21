5569f411ba7ca333f396ef81ecc54aaee3114bd5
mashingan <rahmat.d.ruffy@gmail.com>
2020-08-22 03:46:45 +0700

    add initial support for test replication and ssl

--- tests/testutils.nim
+++ tests/testutils.nim
@@ -29,6 +29,7 @@ const
 
   mongourl {.strdefine, used.} = &"""mongo://rdruffy:rdruffy@localhost:27017/?tlscertificateKeyfile=certificate:{encodeUrl(cert)},key:{encodeUrl(key)}&authSource=admin"""
   verbose = defined(verbose)
+  testReplication* {.booldefine.} = true
 
 when verbose:
   import times, strformat
@@ -95,6 +96,79 @@ template reasonedCheck*(b: BsonDocument | bool, label: string, reason = "") =
     check b
     if not b: (label & ": ").tell reason
 
-when isMainModule:
-  let mongo = newMongo(parseUri mongourl, poolconn = 1)
-  dump mongo.query
+when testReplication:
+  import endians, net, streams
+  import dnsclient
+  # reuse private dnsclient implementation
+  from private/protocol as dnsprot import parseResponse, toStream
+  from private/utils as dnsutils import writeShort
+
+  proc writeName(s: StringStream, srv: SRVRecord, server: string) =
+    for sdot in server.split('.'):
+      s.write sdot.len.byte
+      s.write sdot
+    s.write 0x00.byte
+
+  proc serialize(s: StringStream, srv: SRVRecord, server: string) =
+    srv.rdlength = srv.priority.sizeof + srv.weight.sizeof +
+      srv.port.sizeof
+    let domsrv = server.split('.')
+    for sdot in domsrv:
+      srv.rdlength += byte.sizeof.uint16 + sdot.len.uint16
+    srv.rdlength += byte.sizeof.uint16
+    s.writeName srv, srv.name
+    s.writeShort srv.kind.uint16
+    s.writeShort srv.class.uint16
+
+    var ttl: int32
+    bigEndian32(addr ttl, addr srv.ttl)
+    s.write ttl
+
+    s.writeShort srv.rdlength
+    s.writeShort srv.priority
+    s.writeShort srv.weight
+    s.writeShort srv.port
+    s.writeName srv, server
+    s.setPosition(0)
+
+  proc serialize(s: StringStream, srvs: seq[SRVRecord], server: string) =
+    for srv in srvs:
+      s.serialize srv, server
+
+  proc serialize(data: string): StringStream =
+    var req = data.newStringStream.parseResponse
+    req.header.qr = QR_RESPONSE
+    req.header.ancount = 1
+    req.header.rcode = 0
+    var head = req.header
+    result = head.toStream()
+    req.question.kind = SRV
+    req.question.toStream(result)
+    var srvs = newseq[SRVRecord](3)
+    for i in 0 .. 2:
+      srvs[i] = SRVRecord(
+        name: "localhost",
+        class: IN,
+        ttl: 60,
+        kind: SRV,
+        priority: 0,
+        port: uint16(27018+i),
+        target: "localhost",
+        weight: 0)
+    result.serialize(srvs, "localhost")
+
+  proc fakeDnsServer* =
+    var server = newSocket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+    server.bindAddr(Port 27016)
+    var
+      data = ""
+      address = ""
+      senderport: Port
+      length = 64
+    try:
+      discard server.recvFrom(data, length, address, senderport)
+      let restream = serialize data
+      let datares = restream.readAll
+      server.sendTo(address, senderport, datares)
+    except OSError:
+      echo "Error socket.recvFrom(): ", getCurrentExceptionMsg()
