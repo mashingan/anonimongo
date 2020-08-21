4ec81388442a00343c225b47b727f7c3df15153c
mashingan <rahmat.d.ruffy@gmail.com>
2020-08-22 05:10:25 +0700

    move its own setup test for replication db test

--- tests/test_replication_sslcon.nim
+++ tests/test_replication_sslcon.nim
@@ -1,3 +1,139 @@
 import testutils
 
 {.warning[UnusedImport]: off.}
+
+const
+  testReplication {.booldefine.} = false
+
+when testReplication and defined(ssl):
+  import endians, net, streams, os, osproc
+  import dnsclient
+  # reuse private dnsclient implementation
+  from private/protocol as dnsprot import parseResponse, toStream
+  from private/utils as dnsutils import writeShort
+
+  const
+    dnsport {.intdefine.} = 27016
+    keyname {.strdefine.} = "key.pem"
+    certname {.strdefine.} = "cert.pem"
+
+  proc writeName(s: StringStream, srv: SRVRecord, server: string) =
+    for sdot in server.split('.'):
+      s.write sdot.len.byte
+      s.write sdot
+    s.write 0x00.byte
+
+  proc serialize(s: StringStream, srv: SRVRecord, server: string) =
+    srv.rdlength = srv.priority.sizeof + srv.weight.sizeof +
+      srv.port.sizeof
+    let domsrv = server.split('.')
+    for sdot in domsrv:
+      srv.rdlength += byte.sizeof.uint16 + sdot.len.uint16
+    srv.rdlength += byte.sizeof.uint16
+    s.writeName srv, srv.name
+    s.writeShort srv.kind.uint16
+    s.writeShort srv.class.uint16
+
+    var ttl: int32
+    bigEndian32(addr ttl, addr srv.ttl)
+    s.write ttl
+
+    s.writeShort srv.rdlength
+    s.writeShort srv.priority
+    s.writeShort srv.weight
+    s.writeShort srv.port
+    s.writeName srv, server
+    s.setPosition(0)
+
+  proc serialize(s: StringStream, srvs: seq[SRVRecord], server: string) =
+    for srv in srvs:
+      s.serialize srv, server
+
+  proc serialize(data: string): StringStream =
+    var req = data.newStringStream.parseResponse
+    req.header.qr = QR_RESPONSE
+    req.header.ancount = 3
+    req.header.rcode = 0
+    var head = req.header
+    result = head.toStream()
+    req.question.kind = SRV
+    req.question.toStream(result)
+    var srvs = newseq[SRVRecord](3)
+    for i in 0 .. 2:
+      srvs[i] = SRVRecord(
+        name: "localhost",
+        class: IN,
+        ttl: 60,
+        kind: SRV,
+        priority: 0,
+        port: uint16(27018+i),
+        target: "localhost",
+        weight: 0)
+    result.serialize(srvs, "localhost")
+
+  proc fakeDnsServer* =
+    var server = newSocket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+    server.bindAddr(Port dnsport)
+    var
+      data = ""
+      address = ""
+      senderport: Port
+      length = 64
+    try:
+      discard server.recvFrom(data, length, address, senderport)
+      let restream = serialize data
+      let datares = restream.readAll
+      server.sendTo(address, senderport, datares)
+    except OSError:
+      echo "Error socket.recvFrom(): ", getCurrentExceptionMsg()
+
+  proc getMongoTemp: string =
+    getTempDir()  / "mongotemp"
+  
+  proc createMongoTemp = 
+    createDir getMongoTemp()
+
+  proc createSSLCert: bool =
+    var process = startProcess("openssl", args = @[
+      #"req", "-new", "-newkey", "rsa:4096", "-nodes", "-x509", "-days", "365",
+      "req", "-newkey", "rsa:4096", "-nodes", "-x509", "-days", "365",
+      "--outform", "PEM",
+      "-keyout", keyname, "-out", certname,
+      "-subj", "/C=US/ST=Denial/L=Springfield/O=Dis/CN=temp.com"],
+      options = {poUsePath, poStdErrToStdOut, poInteractive, poParentStreams})
+    result = process.waitForExit == 0
+    try:
+      keyname.moveFile(getMongoTemp() / keyname)
+    except OSError:
+      echo "createSSLcert.moveFile: ", getCurrentExceptionMsg()
+      result = false
+
+  proc setupMongoReplication: seq[Process] =
+    result = newseq[Process](3)
+    var arguments = newseq[seq[string]](3)
+    let mongotemp = getMongoTemp()
+    createDir mongotemp
+    for i in 0 .. 2:
+      let repldb = mongotemp / $i
+      createDir repldb
+      arguments[i] = @[
+        "--port", $(dnsport+i),
+        "--dbpath", repldb,
+        "--bind_ip_all",
+        "--sslMode", "requireSSL",
+        "--sslPEMKeyFile", keyname
+        # TODO: add PEM key self-signed
+        ]
+      when defined(windows):
+        let opt = {poUsePath, poStdErrToStdOut, poInteractive, poParentStreams}
+      else:
+        let opt = {poUsePath, poStdErrToStdOut}
+      result[i] = unown startProcess(exe, args = args, options = opt)
+
+  proc cleanup(processes: seq[Process]) =
+    for i, process in processes:
+      kill process
+    try:
+      removeDir getMongoTemp()
+    except OSError:
+      echo getCurrentExceptionMsg()
