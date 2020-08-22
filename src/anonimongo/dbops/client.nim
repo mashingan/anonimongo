d7316ce9807d261df1c580137d3ae7be71d3bcbd
mashingan <rahmat.d.ruffy@gmail.com>
2020-08-22 17:21:34 +0700

    bump version

--- src/anonimongo/dbops/client.nim
+++ src/anonimongo/dbops/client.nim
@@ -25,7 +25,7 @@ when verbose:
 const
   drivername = "anonimongo"
   description = "nim mongo driver"
-  version = "0.2.0"
+  version = "0.3.0"
 
 proc handshake(m: Mongo, isMaster: bool, s: AsyncSocket, db: string, id: int32,
   appname = "Anonimongo client apps"):Future[ReplyFormat] {.async.} =
