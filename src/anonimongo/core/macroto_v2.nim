e9ac7821fd4f184ecfc25caacc631d6d7f64d424
mashingan <rahmat.d.ruffy@gmail.com>
2020-09-03 22:07:50 +0700

    progress for solve distinct and ref conversion

--- src/anonimongo/core/macroto_v2.nim
+++ src/anonimongo/core/macroto_v2.nim
@@ -1,4 +1,4 @@
-import macros, sugar, strformat
+import macros, sugar, strformat, strutils
 
 {.warning[UnusedImport]: off.}
 
@@ -14,19 +14,52 @@ type
     resvar: NimNode
     fieldDef: NimNode
     fieldImpl: NimNode
+    distinctSyms: seq[NimNode]
+    refnode: NimNode
 
-
-template bsonExport*() {.pragma.}
 
 template extractBracketFrom(n: NimNode): untyped =
   n.expectKind nnkBracketExpr
   n[1]
 
-proc assignPrim(resvar, bsonObject, fieldname: NimNode): NimNode =
-  let fieldNameStr = newStrLitNode $fieldname
-  result = quote do:
-    if `fieldNameStr` in `bsonObject`:
-      `resvar`.`fieldname` = `bsonObject`[`fieldNameStr`]
+proc assignPrim(info: NodeInfo): NimNode =
+  let
+    fieldDef = info.fieldDef
+    fieldname = fieldDef[0]
+    fieldNameStr = newStrLitNode $fieldname
+    typeIdent = ident("of" & ($info.fieldDef[1]).capitalizeAscii)
+    resvar = info.resvar
+    bsonObject = info.origin
+    isref = info.refnode.kind != nnkEmpty
+  let
+    assignedNode =
+      if isref:
+        quote do:(`resvar`.`fieldname`[])
+      else:
+        quote do:(`resvar`.`fieldname`)
+    bsonVal =
+      if isref:
+        quote do:(`bsonObject`[`fieldNameStr`].`typeIdent`)
+      else:
+        quote do:(`bsonObject`[`fieldNameStr`])
+  if info.distinctSyms.len == 0:
+    if isref:
+      result = quote do:
+        if `fieldNameStr` in `bsonObject`:
+          new(`resvar`.`fieldname`)
+          `assignedNode` = `bsonVal`
+    else:
+      result = quote do:
+        if `fieldNameStr` in `bsonObject`:
+          `assignedNode` = `bsonVal`
+  else:
+    let primRes = genSym(nskLet, "primRes")
+    let primType = fieldDef[1]
+    let distinctSym = info.distinctSyms[0]
+    result = quote do:
+      if `fieldNameStr` in `bsonObject`:
+        let `primRes`:`primType` = `bsonObject`[`fieldNameStr`]
+        `assignedNode` = `distinctSym`(`primRes`)
 
 proc isPrimitive(node: NimNode): bool =
   node.kind == nnkSym and node.len == 0
@@ -64,28 +97,81 @@ template retrieveSym(n: var NimNode) =
   n = extractFieldName n
   if n.kind == nnkEmpty: continue
 
+proc processDistinctAndRef(n: var NimNode, fieldType: NimNode):
+  (NimNode, seq[NimNode]) =
+  checknode fieldType
+  result[0] = newEmptyNode()
+  if n.kind == nnkDistinctTy:
+    result[1].add fieldType
+  dump result[1].len
+  checknode n
+  while n.kind in {nnkRefTy, nnkDistinctTy}:
+    if n.kind == nnkRefTy:
+      result[0] = n[0]
+      result[1].add n[0]
+    else:
+      result[1].add n[0]
+    n = n[0].getTypeImpl
+
+
+template prepareWhenStmt(w: var NimNode, fieldtype, fieldname: NimNode,
+  isref: NimNode, info: NodeInfo) =
+  if isref.kind == nnkEmpty:
+    let typeStr = "of" & ($fieldtype).capitalizeAscii
+    let bsonVar = nnkBracketExpr.newTree(info.origin, fieldname)
+    let callBsonVar = newCall(typestr, bsonVar)
+    w.add nnkElifBranch.newTree(
+        newCall("compiles", callBsonVar),
+        newAssignment(info.resvar, callBsonVar))
+
 template identDefsCheck(nodeBuilder: var NimNode, nodeInfo: NodeInfo,
   fielddef: NimNode) =
+
+  checknode fielddef
+
+  # preparing with various checkings
   fielddef.passIfIdentDefs
   var fieldname = fielddef[0]
   fieldname.passIfFieldExported
   fieldname.retrieveSym
+  checknode fielddef
   let fieldtype = fielddef[1]
-  let fieldTypeImpl = getTypeImpl fieldtype
+  var fieldTypeImpl = getTypeImpl fieldtype
+
+  let (isref, distinctSyms) = fieldTypeImpl.processDistinctAndRef fieldType
+
+  var newinfo = nodeInfo
+  newinfo.distinctSyms = distinctSyms
+  newinfo.refnode = isref
+
+  var whenhead = nnkWhenStmt.newTree()
+  whenhead.prepareWhenStmt(fieldType, fieldname, isref, nodeInfo)
+
+  # checking the implementation
+  var elseStmt = newStmtList()
   if fieldTypeImpl.isPrimitive:
-    nodeBuilder.add assignPrim(nodeInfo.resvar, nodeInfo.origin, fieldname)
+    newinfo.fieldDef = nnkIdentDefs.newTree(fieldname, fieldTypeImpl,
+      newEmptyNode())
+    if distinctSyms.len > 1:
+      elseStmt.add assignPrim(newinfo)
+    else:
+      elseStmt.add assignPrim(newinfo)
   elif fieldTypeImpl.kind == nnkObjectTy:
-    var info = nodeInfo
-    info.fieldImpl = fieldType.getImpl
-    info.fieldDef = nnkIdentDefs.newTree(fieldname, fieldtype, newEmptyNode())
-    info.resvar = nnkDotExpr.newTree(nodeInfo.resvar, fieldname)
-    nodeBuilder.add assignObj(info)
+    newinfo.fieldImpl = fieldType.getImpl
+    newinfo.fieldDef = nnkIdentDefs.newTree(fieldname, fieldtype, newEmptyNode())
+    newinfo.resvar = nnkDotExpr.newTree(nodeInfo.resvar, fieldname)
+    elseStmt.add assignObj(newinfo)
     discard
   else:
-    echo fieldType.repr, " is not primitive"
+    echo fieldType.repr, " conversion is not available"
     checknode fieldTypeImpl
     checknode fieldType
-    discard
+    echo "==========="
+  if whenhead.len > 0:
+    whenhead.add nnkElse.newTree(elseStmt)
+    nodeBuilder.add whenhead
+  else:
+    nodeBuilder.add elseStmt
 
 proc assignObj(info: NodeInfo): NimNode =
   let
@@ -129,4 +215,6 @@ macro to*(b: untyped, t: typed): untyped =
     identDefsCheck(result, nodeInfo, fielddef)
 
   result.add(quote do: unown(`resvar`))
-  checknode result
+  #checknode result
+
+template bsonExport*() {.pragma.}
