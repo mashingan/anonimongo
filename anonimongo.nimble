79fe509b004899aa2951e850ffceb73e9b4e66ff
mashingan <rahmat.d.ruffy@gmail.com>
2021-02-07 15:12:10 +0700

    bump patch version

--- anonimongo.nimble
+++ anonimongo.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version       = "0.5.0"
+version       = "0.5.2"
 author        = "Rahmatullah"
 description   = "Anonimongo - Another pure Nim Mongo driver"
 license       = "MIT"
--- src/anonimongo/dbops/client.nim
+++ src/anonimongo/dbops/client.nim
@@ -26,7 +26,7 @@ when verbose:
 const
   drivername = "anonimongo"
   description = "nim mongo driver"
-  anonimongoVersion* = "0.5.0"
+  anonimongoVersion* = "0.5.2"
 
 proc handshake(m: Mongo, isMaster: bool, s: AsyncSocket, db: string, id: int32,
   appname = "Anonimongo client apps"):Future[ReplyFormat] {.async.} =
