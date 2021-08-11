28b05a21b6ae42c269efaca45725ba2a01e1ac8d
mashingan <rahmat.d.ruffy@gmail.com>
2021-08-11 15:13:12 +0700

    add support for informative spinner upload

--- examples/uploadfile/public/js/app.js
+++ examples/uploadfile/public/js/app.js
@@ -1,4 +1,4 @@
-function playvideo( filename ) {
+function playvideo( _filename ) {
   let ws = new WebSocket( "ws://localhost:3000/filename" );
   var v = document.querySelector( "#player" );
 }
@@ -6,18 +6,22 @@ function playvideo( filename ) {
 function submit_file() {
   let ws = new WebSocket( "ws://localhost:3000/ws-upload" );
   let filedom = document.querySelector( "#input-field" );
+  let spinner = document.querySelector( ".lds-roller" )
   ws.onmessage = function ( evnt ) {
     let data = evnt.data;
     console.log( data );
     let infonode = document.querySelector( "#upload-info" )
     let needToDisableInfonode = false
     if ( data === "ok" ) {
+      spinner.style.display = "none"
+
       needToDisableInfonode = true;
       infonode.style.display = "block"
       infonode.style.borderColor = "green"
       infonode.style.backgroundColor = "green"
-      infonode.innerHTML = "Upload OK" 
+      infonode.innerHTML = "Upload OK"
     } else if ( data === "error" ) {
+      spinner.style.display = "none"
       needToDisableInfonode = true;
       infonode.style.display = "block"
       infonode.innerHTML = "Upload Failed"
@@ -26,7 +30,7 @@ function submit_file() {
     if ( needToDisableInfonode ) {
       setTimeout( () => {
         let infonode = document.querySelector( "#upload-info" )
-        if (!infonode) { return; }
+        if ( !infonode ) { return; }
         infonode.style.display = "none"
         infonode.style.borderColor = "green"
         infonode.style.backgroundColor = "green"
@@ -34,7 +38,8 @@ function submit_file() {
     }
   }
 
-  ws.onopen = function ( evnt ) {
+  ws.onopen = function ( _evnt ) {
+    spinner.style.display = "block"
     ws.send( filedom.files[ 0 ].name );
     ws.send( filedom.files[ 0 ].slice() );
     // ws.close();
