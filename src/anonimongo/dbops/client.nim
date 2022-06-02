a039bec97faa805797e02fbe7b02b5663a3cd331
mashingan <rahmat.d.ruffy@gmail.com>
2022-06-02 19:23:06 +0700

    update version number

--- src/anonimongo/dbops/client.nim
+++ src/anonimongo/dbops/client.nim
@@ -27,9 +27,9 @@ const
   drivername = "anonimongo"
   description = "nim mongo driver"
 when not defined(anostreamable):
-  const anonimongoVersion* = "0.5.3"
+  const anonimongoVersion* = "0.6.0"
 else:
-  const anonimongoVersion* = "0.5.3-stream"
+  const anonimongoVersion* = "0.6.0-stream"
 
 proc handshake(m: Mongo[AsyncSocket], isMaster: bool, s: AsyncSocket, db: string, id: int32,
   appname = "Anonimongo client apps"):Future[ReplyFormat] {.multisock.} =
