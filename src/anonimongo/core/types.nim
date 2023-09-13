7c515aba7bae7fbd16110af456b699bcba075fbd
inv2004 <inv2004@gmail.com>
2023-09-13 21:35:08 +0300

    fix tests and username proc

--- src/anonimongo/core/types.nim
+++ src/anonimongo/core/types.nim
@@ -535,7 +535,7 @@ proc appname*(m: Mongo): string =
   else:
     result = ""
 
-func username*(m: Mongo): string =
+proc username*(m: Mongo): string =
   m.main.username
 
 proc tailableCursor*(m: Mongo) =
